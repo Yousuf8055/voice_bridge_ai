84cc2ab5971279c8d8b408da73c5c30898dfc95a
Yousuf Shanawaz Mohammed <yousufshanawazmuhammad@gmail.com>
2026-07-20 04:33:46 +0530

    Fixed Vercel config

--- postcss.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  plugins: [
-    require('./frontend/node_modules/tailwindcss')({ config: './tailwind.config.js' }),
-    require('./frontend/node_modules/autoprefixer'),
-  ]
-}
